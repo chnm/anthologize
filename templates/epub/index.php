a75a959e8d94ddca3d0dd0cfd3a26e40de65df67
patrickrashleigh <patrickrashleigh@gmail.com>
2010-07-29 16:59:35 -0400

    Add epub template, again

---
+++ templates/epub/index.php
@@ -0,0 +1,29 @@
+<?php
+
+  error_reporting(0);
+  
+  
+  echo "HELLO";
+  die;
+  
+  include(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php');
+  
+  $projectID = 867;
+  $tei = new TeiDom($projectID);
+  $teiDom = $tei->getTeiDom();
+
+  echo WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php';
+  die();
+  
+  $xslDom = new DOMDocument();
+  $xslDom->load("tei2html.xsl");
+  
+  $proc = new XSLTProcessor();
+  $proc->importStylesheet($xslDom);
+  
+  // header("Content-type: application/xml");
+  // echo $proc->transformToXML($xmlDom);
+   
+  die();
+
+?>
