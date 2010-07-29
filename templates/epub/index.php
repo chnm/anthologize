200c18d24764b9fbab63b5adcf725160bc6c9795
Patrick Murray-John <pgosetti@umw.edu>
2010-07-29 19:20:21 -0400

    dammit git's annoying me now

--- templates/epub/index.php
+++ templates/epub/index.php
@@ -1,28 +1,28 @@
 <?php
 
-  error_reporting(0);
-  
+  //error_reporting(0);
+
   echo "TEST";
   die;
-  
+
   include(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php');
-  
+
   $projectID = 867;
   $tei = new TeiDom($projectID);
   $teiDom = $tei->getTeiDom();
 
-  echo WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php';
-  die();
-  
+  //echo WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php';
+  //die();
+
   $xslDom = new DOMDocument();
   $xslDom->load("tei2html.xsl");
-  
+
   $proc = new XSLTProcessor();
   $proc->importStylesheet($xslDom);
-  
+
   // header("Content-type: application/xml");
   // echo $proc->transformToXML($xmlDom);
-   
+
   die();
 
 ?>
