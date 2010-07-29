9e66b8d04ebb365da4d5676dbb00faf6a8de685f
Patrick Murray-John <pgosetti@umw.edu>
2010-07-28 20:51:17 -0400

    fine, git, I'll add more files

--- templates/tei/base.php
+++ templates/tei/base.php
@@ -1,9 +1,18 @@
 <?php
 
-$tei = file_get_contents(dirname( __FILE__ ) . '/teiBase.xml');
+$book = new WP_Query(array('id'=>867, 'post_type'=>'books'));
+$partsData = new WP_Query(array('post_parent'=>867, 'post_type'=>'parts'));
+$libraryParts = new WP_Query(array('post_parent'=>868, 'post_type'=>'library_items'));
 
-header('Content-type: application/xml');
-echo $tei;
-//echo "xml someday";
+$author = get_userdata(1);
+//$bookData = query_posts(array('child_o'=>867));
+
+print_r($author);
+
+print_r($book->post);
+//var_dump($bookData);
+print_r($partsData->posts);
+
+
 die();
 ?>
