584917bec473acb3df9e51fa14c1809093a5d944
Patrick Murray-John <pgosetti@umw.edu>
2010-07-29 21:39:50 -0400

    I don't care anymore

--- templates/tei/base.php
+++ templates/tei/base.php
@@ -5,9 +5,9 @@ error_reporting(0);
 
 include(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . "anthologize" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-tei-dom.php');
 
-$projectID = 867;
+$projectID = 877;
 $tei = new TeiDom($projectID);
-$teiDom = $tei->getTeiDom();
-
+//$teiDom = $tei->getTeiDom();
+echo $tei->getTeiString();
 die();
 
