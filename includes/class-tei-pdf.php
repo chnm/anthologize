da1575195875e2dcdf6ae5ae8fddd7408618aca8
Stephen Ramsay <sramsay.unl@gmail.com>
2010-07-31 08:17:14 -0400

    Added license files to PDF translator files.

--- includes/class-tei-pdf.php
+++ includes/class-tei-pdf.php
@@ -1,3 +1,27 @@
+// TeiPdf - Generates PDF from internal, hybridized TEI.
+//
+// This file is part of Anthologize
+//
+// Written and maintained by Stephen Ramsay <sramsay.unl@gmail.com>
+//
+// Last Modified: Sat Jul 31 08:14:13 EDT 2010
+//
+// Copyright (c) 2010 Center for History and New Media, George Mason
+// University.
+//
+// TeiPdf is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option) any
+// later version.
+//
+// TeiPdf is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TeiPdf; see the file COPYING.  If not see
+// <http://www.gnu.org/licenses/>.
 <?php
 
 include_once(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'anthologize' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'tcpdf' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . 'eng.php');
--- templates/pdf/base.php
+++ templates/pdf/base.php
@@ -1,3 +1,27 @@
+// base.php - Controller file for PDF generator.
+//
+// This file is part of Anthologize.
+//
+// Written and maintained by Stephen Ramsay <sramsay.unl@gmail.com>
+//
+// Last Modified: Sat Jul 31 08:16:10 EDT 2010
+//
+// Copyright (c) 2010 Center for History and New Media, George Mason
+// University.
+//
+// Anthologize is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option) any
+// later version.
+//
+// Anthologize is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Anthologize; see the file COPYING.  If not see
+// <http://www.gnu.org/licenses/>.
 <?php
 
 error_reporting(0);
