122814e76e692dcbd2ebdbd31d3f3bfd98348801
Boone Gorges <boonebgorges@gmail.com>
2010-08-02 16:18:23 -0400

    Saves metadata attached to a project during the export process (dedication, license info, and so on) so that it does not need to be rewritten each time you do an export of that project

--- includes/class-export-panel.php
+++ includes/class-export-panel.php
@@ -14,7 +14,33 @@ class Anthologize_Export_Panel {
 	}
 
 	function display() {
-	?>
+		if ( isset( $_GET['project_id'] ) )
+			$options = get_post_meta( $_GET['project_id'], 'anthologize_meta', true );
+
+		if ( !$cdate = $options['cdate'] )
+			$cdate = date('Y');
+
+		if ( !$cname = $options['cname'] )
+			$cname = $options['author_name'];
+
+		// Default is Creative Commons
+		if ( !$ctype = $options['ctype'] )
+			$ctype = 'cc';
+
+		if ( !$cctype = $options['cctype'] )
+			$cctype = 'by';
+
+		// No default for edition number
+		$edition = $options['edition'];
+
+		if ( !$authors = $options['authors'] )
+			$authors = $options['author_name'];
+
+		$dedication = $options['dedication'];
+
+		$acknowledgements = $options['acknowledgements'];
+
+		?>
 		<div class="wrap anthologize">
 
 		<div id="anthologize-logo"><img src="<?php echo WP_PLUGIN_URL . '/anthologize/images/anthologize-logo.gif' ?>" /></div>
@@ -46,39 +72,39 @@ class Anthologize_Export_Panel {
 			<table class="form-table">
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Year', 'anthologize' ) ?></th>
-					<td><input type="text" id="cyear" name="cyear" /></td>
+					<td><input type="text" id="cyear" name="cyear" value="<?php echo $cdate ?>"/></td>
 				</tr>
 
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Copyright Holder', 'anthologize' ) ?></th>
-					<td><input type="text" id="cname" name="cname" value="<?php  ?>" /></td>
+					<td><input type="text" id="cname" name="cname" value="<?php echo $cname ?>" /></td>
 				</tr>
 
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Type', 'anthologize' ) ?></th>
 					<td>
-						<input type="radio" id="ctype" name="ctype" value="c" /> <?php _e( 'Copyright', 'anthologize' ) ?><br />
-						<input type="radio" id="ctype" name="ctype" value="cc" checked="checked" /> <?php _e( 'Creative Commons', 'anthologize' ) ?>
+						<input type="radio" id="ctype" name="ctype" value="c" <?php if ( $ctype == 'c' ) echo 'checked="checked"' ?>/> <?php _e( 'Copyright', 'anthologize' ) ?><br />
+						<input type="radio" id="ctype" name="ctype" value="cc" checked="checked" <?php if ( $ctype != 'c' ) echo 'checked="checked"' ?>/> <?php _e( 'Creative Commons', 'anthologize' ) ?>
 							<select id="cctype" name="cctype">
 								<option value=""><?php _e( 'Select One...', 'anthologize' ) ?></option>
-								<option value="by" selected="selected"><?php _e( 'Attribution', 'anthologize' ) ?></option>
-								<option value="by-sa"><?php _e( 'Attribution Share-Alike', 'anthologize' ) ?></option>
-								<option value="by-nd"><?php _e( 'Attribution No Derivatives', 'anthologize' ) ?></option>
-								<option value="by-nc"><?php _e( 'Attribution Non-Commercial', 'anthologize' ) ?></option>
-								<option value="by-nc-sa"><?php _e( 'Attribution Non-Commercial Share Alike', 'anthologize' ) ?></option>
-								<option value="by-nc-nd"><?php _e( 'Attribution Non-Commercial No Derivatives', 'anthologize' ) ?></option>
+								<option value="by" <?php if ( $cctype == 'by' ) echo 'selected="selected"' ?>><?php _e( 'Attribution', 'anthologize' ) ?></option>
+								<option value="by-sa" <?php if ( $cctype == 'by-sa' ) echo 'selected="selected"' ?>><?php _e( 'Attribution Share-Alike', 'anthologize' ) ?></option>
+								<option value="by-nd" <?php if ( $cctype == 'by-nd' ) echo 'selected="selected"' ?>><?php _e( 'Attribution No Derivatives', 'anthologize' ) ?></option>
+								<option value="by-nc" <?php if ( $cctype == 'by-nc' ) echo 'selected="selected"' ?>><?php _e( 'Attribution Non-Commercial', 'anthologize' ) ?></option>
+								<option value="by-nc-sa" <?php if ( $cctype == 'by-nc-sa' ) echo 'selected="selected"' ?>><?php _e( 'Attribution Non-Commercial Share Alike', 'anthologize' ) ?></option>
+								<option value="by-nc-nd" <?php if ( $cctype == 'by-nc-nd' ) echo 'selected="selected"' ?>><?php _e( 'Attribution Non-Commercial No Derivatives', 'anthologize' ) ?></option>
 							</select>
 					</td>
 				</tr>
 
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Edition', 'anthologize' ) ?></th>
-					<td><input type="text" id="edition" name="edition" /></td>
+					<td><input type="text" id="edition" name="edition" value="<?php echo $edition ?>" /></td>
 				</tr>
 
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Add Author(s)', 'anthologize' ) ?></th>
-					<td><textarea id="authors" name="authors"><?php echo get_post_meta( get_the_ID(), 'author_name', true ) ?></textarea></td>
+					<td><textarea id="authors" name="authors"><?php echo $authors ?></textarea></td>
 				</tr>
 			</table>
 
@@ -89,23 +115,25 @@ class Anthologize_Export_Panel {
 
 			<?php elseif ( $_POST['export-step'] == 1 ) : ?>
 
+			<?php anthologize_save_project_meta() ?>
+
 			<?php $project_id = $_POST['project_id']; ?>
 			<?php $project = get_post( $project_id ); ?>
 
 			<form action="admin.php?page=anthologize/includes/class-export-panel.php&project_id=<?php echo $project_id ?>&noheader=true" method="post">
 
-				<?php _e( 'Title', 'anthologize' ) ?> <input type="text" name="post-title" id="post-title" value="<?php echo $project->post_title ?>" size="100"/>
+				<?php _e( 'Title', 'anthologize' ) ?> <input type="text" name="post-title" id="post-title" value="<?php echo $project->post_title ?>" disabled="disabled" size="100"/>
 
 				<div style="clear: both;"> </div><br />
 
 				<div style="width: 400px; float: left;">
 					<p><strong><?php _e( 'Dedication', 'anthologize' ) ?></strong></p>
-					<textarea id="dedication" name="dedication" cols=35 rows=15></textarea>
+					<textarea id="dedication" name="dedication" cols=35 rows=15><?php echo $dedication ?></textarea>
 				</div>
 
 				<div style="width: 400px; float: left;">
 					<p><strong><?php _e( 'Acknowledgements', 'anthologize' ) ?></strong></p>
-					<textarea id="acknowledgements" name="acknowledgements" cols=35 rows=15></textarea>
+					<textarea id="acknowledgements" name="acknowledgements" cols=35 rows=15><?php echo $acknowledgements ?></textarea>
 				</div>
 
 
@@ -181,7 +209,10 @@ class Anthologize_Export_Panel {
 
 			<?php elseif ( $_POST['export-step'] == 2 ) : ?>
 				<!-- Where the magic happens -->
-				<?php $this->load_template() ?>
+				<?php /* You should never actually get to this point. Method load_template() in anthologize.php should grab all requests with $_POST['filetype'], send a file to the user, and die. If someone ends up here, it means that something has gone awry. */ ?>
+				<p>
+
+				<?php /* $this->load_template() */ ?>
 			<?php endif; ?>
 
 			</div>
@@ -189,6 +220,7 @@ class Anthologize_Export_Panel {
 		<?php
 
 	}
+
 
 	function load_template() {
 		// The goggles! Zey do nossing!
@@ -218,5 +250,6 @@ if ( isset( $_GET['project_id'] ) )
 
 $export_panel = new Anthologize_Export_Panel( $project_id );
 $export_panel->display();
+
 
 ?>
