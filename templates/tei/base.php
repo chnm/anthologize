e755625228fedd50623c11eab9c3d525ed39fd4b
Patrick Murray-John <pgosetti@umw.edu>
2010-08-04 15:44:13 -0400

    fixed debugging stuff in tei/base.phph

--- templates/tei/base.php
+++ templates/tei/base.php
@@ -12,8 +12,8 @@ $ext = "xml";
 
 
 
-//header("Content-type: application/xml");
-//header("Content-Disposition: attachment; filename=$fileName.$ext");
+header("Content-type: application/xml");
+header("Content-Disposition: attachment; filename=$fileName.$ext");
 echo $tei->getTeiString();
 
 
