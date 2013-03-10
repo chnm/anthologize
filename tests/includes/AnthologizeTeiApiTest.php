23b43b5b156dd15be37ce2311234c106723cec7e
Boone B Gorges <boonebgorges@gmail.com>
2013-03-10 00:05:30 -0500

    Introduces Project factory and Anthologize_UnitTestCase

--- tests/includes/AnthologizeTeiApiTest.php
+++ tests/includes/AnthologizeTeiApiTest.php
@@ -4,10 +4,11 @@ define('TEI', 'http://www.tei-c.org/ns/1.0');
 define('HTML', 'http://www.w3.org/1999/xhtml');
 define('ANTH', 'http://www.anthologize.org/ns');
 
-
-class AnthologizeTeiApiTest extends WP_UnitTestCase {
+class AnthologizeTeiApiTest extends Anthologize_UnitTestCase {
 
     public function setUp() {
+	parent::setUp();
+
 	if ( ! class_exists( 'TeiApi' ) ) {
 		require( ANTHOLOGIZE_TEIDOMAPI_PATH );
 	}
@@ -16,7 +17,9 @@ class AnthologizeTeiApiTest extends WP_UnitTestCase {
     	    require_once( ANTHOLOGIZE_TEIDOM_PATH );
         }
 
-        $tei = new DomDocument();
+	$project_id = $this->factory->project->create();
+	$tei = new TeiDom( array( 'project_id' => $project_id ) );
+//        $tei = new DomDocument();
         $tei->load(ANTHOLOGIZE_TESTS_PATH . '/includes/test.xml');
         $teiapi = new TeiApi($tei);
         $teiapi->tei = $tei;
